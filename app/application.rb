a5824f1460e77186348122396c8e6b1bd28bdee9
Daniel Suarez <suarez.daniel101@gmail.com>
2021-10-06 18:31:44 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
     resp = Rack::Response.new
     time = Time.new
 
-    if rand(time.hour) < 12
-      resp.write "Good Morning!"
-    else 
+    if rand(time.hour) >= 12
       resp.write "Good Afternoon!"
+    else 
+      resp.write "Good Morning!"
     end
     resp.finish
   end
