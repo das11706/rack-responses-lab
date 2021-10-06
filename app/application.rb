2146afc0850cab3b7dab66d8dac8553a5ccbe947
Daniel Suarez <suarez.daniel101@gmail.com>
2021-10-06 18:47:37 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,12 +2,12 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
-    time = Time.new
+    time = Time.now
 
-    if rand(time.hour) >= 12
-      resp.write "Good Afternoon!"
-    else 
+    if time.hour < 12
       resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
